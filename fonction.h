af39d71ae3a5b0b23f19d760c63d9b5e4ed35830
zr1b1 <48069991+zr1b1@users.noreply.github.com>
2019-04-11 06:21:03 -0700

    Add files via upload

--- fonction.h
+++ fonction.h
@@ -1,7 +1,17 @@
 #ifndef FONCTION_INCLUDED
 #define FONCTION_INCLUDED
 
-int Alea();
-int Enigme3 ();
+typedef struct 
+{
+    char Question1[200], Answer1[200], Question2[200], Answer2[200], Question3[200], Answer3[200] ;
+    SDL_Rect Question1Pos, Answer1Pos, Question2Pos, Answer2Pos, Question3Pos, Answer3Pos;
+    TTF_Font *Font;
+} Enigme;
 
-#endif // FONCTION_INCLUDED
+void Replace_Hyphen(char *str);
+Enigme init();
+Enigme Alea(Enigme e);
+void afficher_Enigme(SDL_Surface* ecran, SDL_Surface *Cadre, Enigme e);
+int Enigme2 ();
+
+ #endif // FONCTION_INCLUDED
--- main.c
+++ main.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
-#include <time.h>
+#include <SDL/SDL_ttf.h>
+#include <string.h>
 #include "fonction.h"
 
 int main()
 {
+int resultat;
+
+Enigme e,tmp;
 SDL_Init(SDL_INIT_VIDEO);
+TTF_Init();
+TTF_Font *font=TTF_OpenFont("seoepr.ttf",20);
+SDL_Surface *ecran;
+SDL_Surface *Cadre;
 
-SDL_Surface *ecran = NULL, *fond = NULL;
-    SDL_Rect positionFond ;
-    SDL_Event event;
-    int resultat;
-    positionFond.x = 0;
-    positionFond.y = 0;
+e=init(); // initialisation de l enigme
+afficher_Enigme(ecran, Cadre, tmp); // affichage de l enigme generee
+resultat = Enigme2(); // 0 si juste | 1 si fausse
 
-    resultat=Enigme3(); //1 si elle est fausse  / 0 si elle est juste 
-
-
-
- }
+}
