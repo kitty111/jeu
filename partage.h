63571bbd156ff7ca112ed4da96feb50c144b6864
medbannour <48069856+medbannour@users.noreply.github.com>
2019-05-09 12:30:44 +0100

    Add files via upload

---
+++ partage.h
@@ -0,0 +1,28 @@
+/**
+* @struct partage
+* @brief struct for partage
+*/
+#include <SDL/SDL.h>
+
+
+
+
+
+/**
+* @brief To animate right direction  .
+* @param scren the screen
+* @param b the background
+* @return Nothing
+*/
+void walk_anim(SDL_Surface *anim[]);
+/**
+* @brief To load left direction .
+* @param scren the screen
+* @param b the background
+* @return Nothing
+*/
+void walk_back(SDL_Surface *anim[]);
+/**
+@ file [partage.c]
+
+/*
